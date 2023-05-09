c908fbbdd94cb8085df3f77129c4cdbeb626a624
Evandromck <evandroaraujomck@gmail.com>
2023-05-09 14:05:17 -0300

    fix: insert bot

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from email.mime import image
 from tkinter import *
 from tkinter import Tk, StringVar, ttk
 
+import webview
+
+# importando Pillow
 from PIL import Image, ImageTk
+
+# importando Tkcalendar
+from tkcalendar import Calendar, DateEntry
+from datetime import date
 
 # cores
 
@@ -46,13 +53,13 @@ app_img = Image.open('inventorio.png')
 app_img = app_img.resize((45, 45))
 app_img = ImageTk.PhotoImage(app_img)
 
-app_logo = Label(frameCima, image=app_img, text=' Controle', width=900,
+app_logo = Label(frameCima, image=app_img, text=' *', width=900,
                  compound=LEFT, relief=RAISED, anchor=NW, font=('Verdana 20 bold'), bg=co1, fg=co4)
 app_logo.place(x=0, y=0)
 
 # trabalhando no frame Meio
 
-#  criando entradas
+""" ---------------------------Criando entradas---------------------------------------- """
 
 l_nome = Label(frameMeio, text='Nome', height=1, anchor=NW,
                font=('Ivy 10 bold'), bg=co1, fg=co4)
@@ -62,7 +69,7 @@ e_nome = Entry(frameMeio, width=30, justify='left', relief=SOLID)
 e_nome.place(x=130, y=11)
 
 
-l_local = Label(frameMeio, text='Sala', height=1, anchor=NW,
+l_local = Label(frameMeio, text='Local', height=1, anchor=NW,
                 font=('Ivy 10 bold'), bg=co1, fg=co4)
 l_local.place(x=10, y=40)
 
@@ -70,20 +77,92 @@ e_local = Entry(frameMeio, width=30, justify='left', relief=SOLID)
 e_local.place(x=130, y=41)
 
 
-l_nome = Label(frameMeio, text='Nome', height=1, anchor=NW,
-               font=('Ivy 10 bold'), bg=co1, fg=co4)
-l_nome.place(x=10, y=10)
+l_desecricao = Label(frameMeio, text='Desecrição', height=1, anchor=NW,
+                     font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_desecricao.place(x=10, y=70)
 
-e_nome = Entry(frameMeio, width=30, justify='left', relief=SOLID)
-e_nome.place(x=130, y=11)
+e_desecricao = Entry(frameMeio, width=30, justify='left', relief=SOLID)
+e_desecricao.place(x=130, y=71)
 
 
-l_nome = Label(frameMeio, text='Nome', height=1, anchor=NW,
-               font=('Ivy 10 bold'), bg=co1, fg=co4)
-l_nome.place(x=10, y=10)
+l_model = Label(frameMeio, text='Model', height=1, anchor=NW,
+                font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_model.place(x=10, y=100)
 
-e_nome = Entry(frameMeio, width=30, justify='left', relief=SOLID)
-e_nome.place(x=130, y=11)
+e_model = Entry(frameMeio, width=30, justify='left', relief=SOLID)
+e_model.place(x=130, y=101)
+
+""" ---------------------------Calandario---------------------------------------- """
+
+l_cal = Label(frameMeio, text='Data da compra', height=1, anchor=NW,
+              font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_cal.place(x=10, y=130)
+e_cal = DateEntry(frameMeio, width=12, Background='darkblue',
+                  bordewidht=2, year=2023)
+e_cal.place(x=130, y=131)
+
+""" ---------------------------valor-------------------------------------------- """
+
+l_model = Label(frameMeio, text='Valor', height=1, anchor=NW,
+                font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_model.place(x=10, y=160)
+
+e_model = Entry(frameMeio, width=30, justify='left', relief=SOLID)
+e_model.place(x=130, y=161)
+
+
+l_model = Label(frameMeio, text='Compra', height=1, anchor=NW,
+                font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_model.place(x=10, y=190)
+
+e_model = Entry(frameMeio, width=30, justify='left', relief=SOLID)
+e_model.place(x=130, y=191)
+
+""" ---------------------------Botoes_imagem-------------------------------------------- """
+
+l_carregar = Label(frameMeio, text='imagen do item', height=1, anchor=NW,
+                   font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_carregar.place(x=10, y=220)
+
+
+b_carregar = Button(frameMeio, width=30, text="carregar".upper(
+), command=CENTER, overrelief=RIDGE, anchor=CENTER, font=('Ivy 8'), bg=co1, fg=co0)
+b_carregar.place(x=130, y=221)
+
+""" ---------------------------BOTAO INSERIR IMAGEM-------------------------------------------- """
+
+
+l_carregar = Label(frameMeio, text='imagen do item', height=1, anchor=NW,
+                   font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_carregar.place(x=10, y=220)
+
+
+b_carregar = Button(frameMeio, width=30, text="carregar".upper(
+), command=CENTER, overrelief=RIDGE, anchor=CENTER, font=('Ivy 8'), bg=co1, fg=co0)
+b_carregar.place(x=130, y=221)
+
+
+""" ---------------------------BOTAO INSERIR ITEM-------------------------------------------- """
+
+img_add = Image.open('Add.png')
+img_add = img_add.resize((20, 20))
+img_add = ImageTk.PhotoImage(img_add)
+
+
+b_inserir = Button(frameMeio, image=img_add, width=95, text='   ADICIONAR'.upper(
+), compound=LEFT, anchor=NW, overrelief=RIDGE, font=('Ivy 8'), bg=co1, fg=co0)
+b_inserir.place(x=330, y=10)
+
+""" ---------------------------BOTAO ATUALIZAR-------------------------------------------- """
+
+img_update = Image.open('update.png')
+img_update = img_update.resize((20, 20))
+img_update = ImageTk.PhotoImage(img_update)
+
+
+b_update = Button(frameMeio, image=img_update, width=95, text='   ADICIONAR'.upper(
+), compound=LEFT, anchor=NW, overrelief=RIDGE, font=('Ivy 8'), bg=co1, fg=co0)
+b_update.place(x=330, y=50)
 
 
 janela.mainloop()
